03f7476fc0dcc4780dec2ff94a7b60ec6f84ec77
Alessandra Vertrees <avertrees447@gmail.com>
2019-06-18 16:18:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,12 +52,6 @@ def monopoly_with_fourth_tier
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
-	    },
 	    :names => {
 	      :reading_railroad => {
 	        "mortgage_value" => "$100"
@@ -72,6 +66,12 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$800"
 	      }
 	    }
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
+	    },
 	  }
 	}
 
